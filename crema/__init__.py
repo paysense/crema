0560bc43838bef0335e28e2ef68c41c3dad9314a
Rohit Laddha <rohitladdha@users.noreply.github.com>
2020-05-24 16:02:39 +0530

    adding async push, allow configs (#12)
    adding async push, allow configs

--- crema/__init__.py
+++ crema/__init__.py
@@ -1,3 +1,4 @@
 from .choices import EventType
 from .exceptions import KafkaException
 from .kafka_util import kafka_util
+from .kafka_util import KafkaUtil
--- crema/kafka_util.py
+++ crema/kafka_util.py
@@ -4,6 +4,7 @@ import logging
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
 
+from crema.decorators import singleton
 from .config import ENABLED, KAFKA_BOOTSTRAP_SERVERS
 from .exceptions import KafkaException
 from .hashing import PartitionHashing
@@ -11,6 +12,7 @@ from .hashing import PartitionHashing
 LOGGER = logging.getLogger("kafka_util")
 
 
+@singleton
 class KafkaUtil:
     """
     This util is responsible for pushing data successfully to Kafka cluster and it also manages the success
@@ -18,22 +20,72 @@ class KafkaUtil:
     on every api call
     """
 
-    def __init__(self):
-        self.producer = None
+    def __init__(self, kafka_vars=None):
+        self._kafka_producer = None
+        self.kafka_vars = kafka_vars or {}
+
+    @property
+    def producer(self):
+        # initialise kafkaProducer only when its about to send events. It avoids creating unnecessary connection
+        # to kafka cluster.
+        if self._kafka_producer is None:
+            self._kafka_producer = KafkaProducer(
+                bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
+                value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+                api_version=(8,),
+                **self.kafka_vars
+            )
+        return self._kafka_producer
+
+    def _success_callback(self, data, record_metadata):
+        # not using f"" style as python 3.4 is used at AMS and doesn't support this style
+        LOGGER.debug(
+            (
+                "Successfully published data: {data}, with topic: {topic}, on partition: "
+                "{partition} with offset: {offset}"
+            ).format(
+                data=data,
+                topic=record_metadata.topic,
+                partition=record_metadata.partition,
+                offset=record_metadata.offset,
+            )
+        )
+
+    def _error_callback(self, exception, data, partition):
+        if isinstance(exception, KafkaError):
+            msg = "{e}, partition: {partition}, data: {data}".format(
+                e=str(exception), partition=partition, data=data
+            )
+            LOGGER.exception(msg)
+            raise KafkaException(msg)
+        else:
+            raise exception
+
+    def push_async(self, data):
+        """
+        It pushed data asynchronously to kafka servers. It is useful when data is sent in a api call.
+        Args:
+            data:
+
+        Returns:
+
+        """
+        if ENABLED is False:
+            LOGGER.info("Please set ENABLE_KAFKA env variable to True to push events")
+            return
+
+        master_user_id = data["meta_data"]["user_id"]
+        event_type = data["meta_data"]["event_type"]
+        partition = PartitionHashing.get_partition(master_user_id, event_type)
+
+        self.producer.send(event_type, data, partition=partition,).add_callback(
+            self._success_callback, data
+        ).add_errback(self._error_callback, data, partition)
 
     def push(self, data):
         if ENABLED is False:
             LOGGER.info("Please set ENABLE_KAFKA env variable to True to push events")
             return
-
-        if self.producer is None:
-            # initialise kafkaProducer only when its about to send events. It avoids creating unnecessary connection
-            # to kafka cluster.
-            self.producer = KafkaProducer(
-                bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
-                value_serializer=lambda v: json.dumps(v).encode("utf-8"),
-                api_version=(8,),
-            )
 
         master_user_id = data["meta_data"]["user_id"]
         event_type = data["meta_data"]["event_type"]
@@ -62,4 +114,5 @@ class KafkaUtil:
             raise KafkaException(msg)
 
 
+# todo should deprecate below when all the clients have update the lib versin
 kafka_util = KafkaUtil()
