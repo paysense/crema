610d588c660552ce819efa251a514d95886f59de
Rohit Laddha <rohitladdh@gmail.com>
2020-05-26 17:47:47 +0530

    fixup! fixing config variablese

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='crema',
-    version='0.14',
+    version='0.15',
     description='Kafka Library',
     url='https://github.com/paysense/crema',
     author='Rohit Laddha',
