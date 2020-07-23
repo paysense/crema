8907c51982c3c698c572c5ef90ba8f703eca921b
akshaywattamwar01 <60779173+akshaywattamwar01@users.noreply.github.com>
2020-07-23 21:56:00 +0530

    Hotfix/partition count change (#26)
    partition changes for events

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='crema',
-    version='1.5',
+    version='1.6',
     description='Kafka Library',
     url='https://github.com/paysense/crema',
     author='Rohit Laddha',
