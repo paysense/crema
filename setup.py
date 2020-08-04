fef68d555513e17378217cfd5ca8eae4355d7804
akshaywattamwar01 <60779173+akshaywattamwar01@users.noreply.github.com>
2020-08-04 11:41:13 +0530

    Entity user agent assignemnt (#29)
     user agent assignment entity added

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='crema',
-    version='1.8',
+    version='1.9',
     description='Kafka Library',
     url='https://github.com/paysense/crema',
     author='Rohit Laddha',
