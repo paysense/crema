bcfa97fa996dbcb3697b50384efbab2566a0c65f
Rohit Laddha <rohitladdha@users.noreply.github.com>
2020-09-15 17:18:25 +0530

    add topic Nbfc assessment (#33)
    add NBFC_ASSESSMENT topic

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='crema',
-    version='2.2',
+    version='2.3',
     description='Kafka Library',
     url='https://github.com/paysense/crema',
     author='Rohit Laddha',
