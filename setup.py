23dc715fcb3679cf5506342d1824851944fa9c82
mohit.kaushal <mohit.kaushal@paysense.in>
2021-03-23 20:03:44 +0530

    add events for comms from pscore and for repayment from payments

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='crema',
-    version='2.8',
+    version='2.9',
     description='Kafka Library',
     url='https://github.com/paysense/crema',
     author='Rohit Laddha',
