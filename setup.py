ed532ed6d8bc0e3260338b0e5ab9b41cd23de944
akshaywattamwar01 <60779173+akshaywattamwar01@users.noreply.github.com>
2022-04-28 19:09:55 +0530

    Revert "COL-3521 - adding changes for CUSTOMER_REASSESSMENT, updating crema v…"

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='crema',
-    version='2.21',
+    version='2.20',
     description='Kafka Library',
     url='https://github.com/paysense/crema',
     author='Rohit Laddha',
