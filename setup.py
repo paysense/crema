d1f4262e35ba58c12a413c07a9255d56988fcadd
Siddharth Peyyeti <siddharth.peyyeti@payufin.com>
2022-04-28 16:57:47 +0530

    Updating crema version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='crema',
-    version='2.19',
+    version='2.21',
     description='Kafka Library',
     url='https://github.com/paysense/crema',
     author='Rohit Laddha',
