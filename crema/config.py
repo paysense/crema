c8d912a381549d1efd1f2eec1e7f5691ff537dc3
ANKITSG15 <39673198+ANKITSG15@users.noreply.github.com>
2022-09-02 12:50:19 +0530

    Update config.py (#60)
    Correct the documentation

--- crema/config.py
+++ crema/config.py
@@ -4,7 +4,8 @@ import os
 kafka_servers = os.environ.get("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
 KAFKA_BOOTSTRAP_SERVERS = kafka_servers.split(",")
 
-# flag to enable/disable kafka logging
+# flag to enable/disable kafka on a particular service that is using this library,
+# this will give control after integration where the service can decide to stop the kafka message push with this flag
 ENABLED = os.environ.get("ENABLE_KAFKA", "False").lower() == 'true'
 
 # wait time to get response from kafka
